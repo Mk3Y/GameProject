f701e2dd3c9a174ef9830a34d36d8577d04c44d2
mk3y <mk3y@hotmail.it>
2015-07-07 16:01:41 +0200

    Added src folder

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @author Mirko Nava
+ * @version 0.1
+ * Created by Mirko Nava - mirkna@gmail.com
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //insert code here
+    }
+
+}
